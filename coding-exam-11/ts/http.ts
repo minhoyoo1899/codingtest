bb24443f32faf703561a614f39620d3e4dc31b57
MinhoYoo <minhoyoo1899@gmail.com>
2022-11-29 10:51:07 +0900

    디렉토리 생성후 파일쓰기 읽기를 동기처리로 하려했으나 순서대로 되지 않음 콜백을 써봐도 안됨 무슨 방법이 있을지 생각해 봐야 할듯 그리고 디렉토리 생성했으면 덮어씌우기가 안되고 에러가 남

--- coding-exam-11/ts/http.ts
+++ coding-exam-11/ts/http.ts
@@ -3,6 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const url = require('url');
 
+// fs.mkdirSync("../routes", (err: any) => {
+//   if (err) throw err;
+//   DynamicMakeServer(header, main, footer);
+// });
+
 //read
 const head = fs.readFileSync('../txt/head.txt', 'utf-8', (err: any) => {
   if (err) throw err;
@@ -18,9 +23,9 @@ const main = fs.readFileSync('../txt/body.txt', 'utf-8', (err: any) => {
 
 const readBody = () => {
   const bodyText = fs.readFileSync('../txt/body.txt', 'utf-8', (err: any) => {
-  if (err) throw err;
-  });  
-  
+    if (err) throw err;
+  });
+
   return bodyText;
 }
 
@@ -29,10 +34,18 @@ const footer = fs.readFileSync('../txt/footer.txt', 'utf-8', (err: any) => {
 });
 
 const DynamicMakeServer = (childItem1: string, childItem2: string, childItem3: string) => {
-  const bodyContext = `${childItem1} ${childItem2} ${childItem3}`; 
+  const bodyContext = `${childItem1} ${childItem2} ${childItem3}`;
+  // const arr = ["a", "b", "c", "d"];
+  // for (let i = 0; i < arr.length; i++) {
+  //   fs.writeFile(`../routes/${arr[i]}.txt`, arr[i], function (err: any) {
+  //     if (err) throw err;
+  //   });
+  // }
+
+
 
   // const returnBody = readBody();
-  
+
   fs.appendFile('../txt/body.txt', bodyContext, (err: any) => {
     if (err) throw err;
     const l_body = readBody();
@@ -40,29 +53,29 @@ const DynamicMakeServer = (childItem1: string, childItem2: string, childItem3: s
     //console.log(l_body);
     combine(header, l_body);
   });
-  
 
-// const l_body:any = fs.readFileSync('../txt/body.txt', 'utf-8', (err: any) => {
-//       if (err) throw err;
-//   }
+
+  // const l_body:any = fs.readFileSync('../txt/body.txt', 'utf-8', (err: any) => {
+  //       if (err) throw err;
+  //   }
 }
 
 // const bodyChanger = async (childItem1: string, childItem2: string, childItem3: string) => {
 //   const bodyContext = `${childItem1} ${childItem2} ${childItem3}`;
 
 //   // const returnBody = readBody();
-  
+
 //    const updatBody = await fs.appendFile('../txt/body.txt', bodyContext, (err: any) => {
 //       if (err) throw err;
 //    }).then(readBody()).then((res: any) => {
 //      console.log(res);
 //     })
-  
+
 
 // // const l_body:any = fs.readFileSync('../txt/body.txt', 'utf-8', (err: any) => {
 // //       if (err) throw err;
 // //   }    
-  
+
 //   return updatBody;
 // }
 
@@ -77,30 +90,72 @@ const DynamicMakeServer = (childItem1: string, childItem2: string, childItem3: s
 // console.log(bodyText);
 
 const combine = (head: string, body: string) => {
-  const html = `<html> <head>${head}</head> <body> ${body} </body></html>`
-  openServer(html);  
+  const routeA = fs.readFileSync('../routes/a.txt', 'utf-8', (err: any) => {
+    if (err) throw err;
+  });
+
+  const routeB = fs.readFileSync('../routes/b.txt', 'utf-8', (err: any) => {
+    if (err) throw err;
+  });
+
+  const routeC = fs.readFileSync('../routes/c.txt', 'utf-8', (err: any) => {
+    if (err) throw err;
+  });
+
+  const routeD = fs.readFileSync('../routes/d.txt', 'utf-8', (err: any) => {
+    if (err) throw err;
+  });
+  const index = `<html> <head>${head}</head> <body> ${body} </body></html>`;
+  const a = `<html> <head>${head}</head> <body> ${routeA} </body></html>`;
+  const b = `<html> <head>${head}</head> <body> ${routeB} </body></html>`;
+  const c = `<html> <head>${head}</head> <body> ${routeC} </body></html>`;
+  const d = `<html> <head>${head}</head> <body> ${routeD} </body></html>`;
+  openServer(index, a, b, c, d);
 }
 
-function openServer(html: any) {
+function openServer(index: string, a: string, b: string, c: string, d: string) {
   const server = http.createServer((req: any, res: any) => {
-    let getMethod = req.method;
-  switch (getMethod) {
-    case 'GET':
-      res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8'});
-      res.write(html);
-      res.end();
-      break;
-    default:
-      res.writeHead(405, { 'Content-Type': 'text/html; charset=UTF-8'});
-      res.write('Method Not Allowed');
-      res.end();
-      break;
-  }
+    let url = req.url;
+    if (req.method === "GET") {
+      switch (url) {
+        case '/':
+          res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+          res.write(index);
+          res.end();
+          break;
+        case '/a':
+          res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+          res.write(a);
+          res.end();
+          break;
+        case '/b':
+          res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+          res.write(b);
+          res.end();
+          break;
+        case '/c':
+          res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+          res.write(c);
+          res.end();
+          break;
+        case '/d':
+          res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+          res.write(d);
+          res.end();
+          break;
+        // default:
+        //   res.writeHead(405, { 'Content-Type': 'text/html; charset=UTF-8' });
+        //   res.write('Method Not Allowed');
+        //   res.end();
+        //   break;
+      }
+    }
   });
-  server.listen(5678, (error: any) => {if(error) throw error});  
+  server.listen(5678, (error: any) => { if (error) throw error });
 }
 
 DynamicMakeServer(header, main, footer);
+
 
 // console.log(combine(,);
 
