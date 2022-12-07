dc27f6c1f570e1435a097673e5d78b902b9dd13c
MinhoYoo <minhoyoo1899@gmail.com>
2022-12-07 11:34:35 +0900

    폴더 정리

---
+++ js/morning.js
@@ -0,0 +1,13 @@
+function employment(object) {
+  // algorithm
+  return {
+    companyName: "",
+    mainStack: [],
+    subStack: [],
+    busunessModel: [] || "",
+    positiveFeather: [],
+    negativeFeather: []
+  }
+}
+
+let hoobo = [];
---
+++ ts/morning_job.ts
@@ -0,0 +1,68 @@
+function employment(object: object) {
+  // algorithm
+  return {
+    companyName: "",
+    mainStack: [],
+    subStack: [],
+    busunessModel: [] || "",
+    positiveFeather: [],
+    negativeFeather: []
+  }
+}
+
+// let hoobo = [];
+const hoobo = new Array();
+
+/**
+ * 회사명 : (주) 신시웨이
+ * mainStack: JAVA, C, javascript, RDBMS
+ * 우대사항: linux, Server,Network
+ * BM : 데이터 베이스 보안전문
+ * positive : 자바 DB 리눅스 부분을 더 알고 싶기도 하고 배울 수 있음
+ * negative : 서울에서 일 할수도 있음, 절차가 상당히 복잡(탄탄한 회사임이 증명됨과 동시에 기대치에 충족이 안되면 인턴만하다 끝날 가능성이 높음)
+ */
+
+/**
+ * 회사명 : (주) 코드마인드
+ * mainStack:ㆍJava , Java 스프링프레임워크(SpringBoot)를 활용한 Web Application 개발 경험 보유, MariaDB 또는 PostgreSQL 등 RDBMS 경험 보유
+ * 우대사항: Git활용, 리눅스 시스템
+ * BM: 소프트웨어개발
+ * positive: Java및 RDBMS활용자 우대
+ * negative: 컴퓨터학 전공자 우대, 
+*/
+
+/**
+ * 회사명 : (주) 메일플러그
+ * mainStack: JAVA, Python, PHP, GO, MAC/Win, JIRA, Git, Agile, Fisheye, Ubuntu
+ * 우대사항: 서버이해도, 자바기반 개발환경 유경험자
+ * BM: 시스템 소프트웨어 개발
+ * positive: 서버, DB업부 지원
+ * negative: 컴퓨터 전공 우대, 정보보관리 자격증 우대
+*/
+
+/**
+ * 회사명 : (주)시스메이트
+ * mainStack:CSS 3  HTML5, Spring Framework,Java, JavaScript
+ * 우대사항: 자격증, 3년이상 경력자 우대
+ * BM: 클라우드, 빅데이터
+ * positive: 이미 해본 업무들
+ * negative: 이미 배워온 업무여서 만일 입사하더라도 빠르게 배울수 있음 하지만 새로운 기술 스택을 쌓기에는 어려움
+*/
+
+/**
+ * 회사명 : (주)앤스시스템
+ * mainStack:unity unityjava javac# c#javascript javascriptspring springc++ c++
+ * 우대사항: 
+ * BM: 게임개발 유지보수
+ * positive: 넷마블 협력 업체
+ * negative: 안드로이드 네이티브앱 유경험자 자격, 상시채용
+*/
+
+/**
+ * 회사명 : 
+ * mainStack:
+ * 우대사항: 
+ * BM: 
+ * positive: 
+ * negative: 
+*/
